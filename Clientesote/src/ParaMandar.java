38a17071ed347b9d9683586560018a68a685c20d
MangoSalazar <l22440193@mochis.tecnm.mx>
2025-10-17 15:52:59 -0700

    cree el metodo enviarTest

--- Clientesote/src/ParaMandar.java
+++ Clientesote/src/ParaMandar.java
@@ -4,35 +4,45 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
-public class ParaMandar implements Runnable{
-    String[] mensajitos = {"@0 hola", "que rollo", "opa opa", "register mango"};
+
+public class ParaMandar implements Runnable {
+
+    String[] mensajitos = {"@0 mensaje privado", "primer mensaje global", "segundo mensaje global", "register mango"};
     final BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
-    final DataOutputStream salida ;
+    final DataOutputStream salida;
+
     public ParaMandar(Socket s) throws IOException {
         this.salida = new DataOutputStream(s.getOutputStream());
     }
 
     @Override
     public void run() {
+        mandarMensaje();
+
+    }
+
+    public void mandarMensaje() {
         int nMensaje = 0;
-        while ( true ){
-            String mensaje;
+        while (true) {
+            String mensaje = "";
             try {
-                while (nMensaje < mensajitos.length) {
-                    mensaje = mensajitos[nMensaje];
-                    if (nMensaje < 4) {
-                        salida.writeUTF(mensaje);                    
-                    }
-                    nMensaje++;
-                }
+                //De momento creare el metodo de enviar test para provar el cliente y el servidor
+                nMensaje = enviarTest(nMensaje, mensaje);
             } catch (IOException ex) {
             }
 
         }
     }
-    public static void mandarMensaje(String mensaje){
-    
+
+    public int enviarTest(int nMensaje, String mensaje) throws IOException {
+        while (nMensaje < mensajitos.length) {
+            mensaje = mensajitos[nMensaje];
+            if (nMensaje < 4) {
+                salida.writeUTF(mensaje);
+            }
+            nMensaje++;
+        }
+        return nMensaje;
     }
 
 }
- 
